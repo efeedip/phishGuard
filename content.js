02fd2107e5616e81883e7fb87046c1de5effc95b
Efe Edip Ceylani <efeedipceylani@gmail.com>
2023-09-07 22:23:15 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,23 @@
+// Define the string you want to search for
+const searchString = "sube.assets";
+const currentUrl = window.location.href;
+console.log(currentUrl)
+
+// Get the source code of the web page
+const sourceCode = document.documentElement.outerHTML;
+
+// Check if the string is present in the source code
+if (sourceCode.includes(searchString)) {
+  // String is found
+  if (currentUrl.includes("sube.garantibbva.com.tr")) {
+    console.log("Not phishing");
+  } else {
+    console.log("It is phishing")
+  }
+  
+  // You can perform additional actions here
+} else {
+  // String is not found
+  console.log(`"${searchString}" not found in the source code.`);
+  // You can perform alternative actions here
+}
