f31c204e4058a1a9bd845a55e76fcdc8a4ffaca9
Efe Edip Ceylani <efeedipceylani@gmail.com>
2023-09-08 23:38:14 +0300

    changed structre

--- content.js
+++ content.js
@@ -23,18 +23,3 @@ if (sourceCode.includes(searchString)) {
 }
 
 
-$.ajax({
-  url: 'https://www.whois.com/whois/' + "garantibbva.com.tr",
-  type: 'GET',
-  success: function (res) {
-    // Parse the response HTML using jQuery
-    const parsedHtml = $(res.responseText);
-
-    // Find the element with the id "registryData" and get its text content
-    const registryDataText = parsedHtml.find("#registryData").text();
-
-    // Log the extracted text to the console
-    console.log("registryData:", registryDataText);
-  }
-});
-
