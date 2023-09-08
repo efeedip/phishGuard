7183251f67d0b0565107a807e50e7d8e11776add
Efe Edip Ceylani <efeedipceylani@gmail.com>
2023-09-08 23:39:38 +0300

    changed structre with changed background.js

--- background.js
+++ background.js
@@ -1,66 +1,21 @@
-/* chrome.runtime.onMessage.addListener(function (message, _sender, sendResponse) {
-  if (message.domain) {
-    const domain = message.domain;
-    const apiUrl = `https://www.whois.com/whois/${domain}`;
+// Listen for the onCompleted event when a page finishes loading
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  // Check if the URL matches the page you want to target (adjust the URL pattern as needed)
 
-    fetch(apiUrl)
-      .then(response => response.text())
-      .then(html => {
-        // Log the response.text() value to the console
-        console.log("Response text from WHOIS API:", html);
+    // Execute your background script logic here
+  const currentUrl = window.location.hostname;
+  const apiUrl = `https://www.whois.com/whois/${currentUrl}`;
 
-        sendResponse({ result: html });
-      })
-      .catch(error => {
-        sendResponse({ result: `Error: ${error.message}` });
-        console.error("Error:", error.message);
+  fetch(apiUrl)
+    .then(response => response.text())
+    .then(html => {
+      // Send the HTML content to the content script
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { htmlContent: html });
       });
-
-    return true; // Indicates that sendResponse will be called asynchronously
-  }
-}); 
-
-
-
-chrome.runtime.onMessage.addListener(function (message, _sender, _sendResponse) {
-  if (message.domain) {
-    const domain = message.domain;
-    const apiUrl = `https://www.whois.com/whois/${domain}`;
-
-    fetch(apiUrl)
-      .then(response => response.text())
-      .then(html => {
-        // Log the response text value to the console
-        console.log("Response text from WHOIS API:", html.includes("registryData"));
-      })
-      .catch(error => {
-        console.error("Error:", error.message);
-      });
-
-    return true; // Indicates that sendResponse will be called asynchronously
-  }
-}); */
-
-chrome.runtime.onMessage.addListener(function (message, _sender, sendResponse) {
-  if (message.domain) {
-    const domain = message.domain;
-    const apiUrl = `https://www.whois.com/whois/${domain}`;
-
-    fetch(apiUrl)
-      .then(response => response.text())
-      .then(html => {
-        // Send the HTML content to the content script
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          const activeTab = tabs[0];
-          chrome.tabs.sendMessage(activeTab.id, { htmlContent: html });
-        });
-      })
-      .catch(error => {
-        console.error("Error:", error.message);
-      });
-  }
+    })
+    .catch(error => {
+      console.error("Error:", error.message);
+    });
 });
-
-
-
-
--- content.js
+++ content.js
@@ -1,13 +1,12 @@
-// Define the string you want to search for
-const searchString = "sube.assets";
-const currentUrl = window.location.href;
-console.log(currentUrl)
+console.log("Content script loaded."); // You can log messages to the console for testing.
+
+const GBassets = "assets.garanti";
 
 // Get the source code of the web page
 const sourceCode = document.documentElement.outerHTML;
 
 // Check if the string is present in the source code
-if (sourceCode.includes(searchString)) {
+if (sourceCode.includes(GBassets)) {
   // String is found
   if (currentUrl.includes("sube.garantibbva.com.tr")) {
     console.log("Not phishing");
@@ -18,8 +17,55 @@ if (sourceCode.includes(searchString)) {
   // You can perform additional actions here
 } else {
   // String is not found
-  console.log(`"${searchString}" not found in the source code.`);
+  console.log("Not phishing");;
   // You can perform alternative actions here
 }
 
+// Define a variable to track whether the script has already run
+let scriptHasRun = false;
 
+// Function to process the message and extract data
+function processMessage(message) {
+
+  if (message.htmlContent) {
+    // Parse the HTML content sent from the background script
+    const html = message.htmlContent;
+
+    // Create a temporary element to parse the HTML
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = html;
+
+    const registryDataBlock = tempElement.querySelector("#registryData");
+
+    /* registry kontrol - domain age check */
+    if (registryDataBlock) {
+      // Log the text inside the 'registryData' element to the console
+      const registryBlockText = registryDataBlock.textContent;
+      const indexofCreation = registryBlockText.indexOf("Creation");
+      const createdOn = registryBlockText.substring(indexofCreation + 15, indexofCreation + 25);
+
+      const date1 = new Date();
+      const date2 = new Date(createdOn);
+      const diffTime = Math.abs(date2 - date1);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      
+      console.log(diffDays + " days");
+      console.log("Created on:", createdOn);
+    } else {
+      console.log("#registryData element not found in the HTML.");
+    }
+  }
+
+  // Set the scriptHasRun variable to true to indicate that the script has run
+  scriptHasRun = true;
+}
+
+// Listen for messages from the background script only if the script hasn't run yet
+if (!scriptHasRun) {
+  chrome.runtime.onMessage.addListener(function (message, _sender, _sendResponse) {
+    // Check if the script has already run before processing the message
+    if (!scriptHasRun) {
+      processMessage(message);
+    }
+  });
+}
