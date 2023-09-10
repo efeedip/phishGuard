83ab05d24c26674cf4c3eb91917b649fe2cc9b9f
Efe Edip Ceylani <efeedipceylani@gmail.com>
2023-09-10 22:55:08 +0300

    risk analysis added, warning notif added based on risk score

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-
-
 chrome.webNavigation.onCompleted.addListener(function (details) {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.showPhishingPageNotification) {
@@ -7,70 +5,85 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
       showPhishingDetectedNotification(message.url);
     }
 
-    if(message.showRealPageNotification) {
+    if (message.showRealPageNotification) {
       // Create and show the notification
       showRealSiteNotification();
+    }
+
+    if (message.showWarningPageNotification) {
+      showWarningNotification();
     }
   });
 });
 
+chrome.notifications.onButtonClicked.addListener(
+  (notificationId, buttonIndex) => {
+    if (buttonIndex === 0) {
+      // Check if the first button was clicked
+      // Send a message to the content script to close the tab
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0]) {
+          chrome.tabs.remove(tabs[0].id);
+        }
+      });
+    }
+  }
+);
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.calculateRisk) {
-    calculateRisk(message.domain_url);
+  if (message.whoisQuery) {
+    whoisQuery(message.domain_url);
   }
 });
 
-
-chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
-  if (buttonIndex === 0) { // Check if the first button was clicked
-    // Send a message to the content script to close the tab
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) {
-        chrome.tabs.remove(tabs[0].id);
-      }
-    });
-  }
-});
-
-
-function calculateRisk(domain_url) {
+function whoisQuery(domain_url) {
   const currentUrl = domain_url;
   const apiUrl = `https://www.whois.com/whois/${domain_url}`;
 
   fetch(apiUrl)
-    .then(response => response.text())
-    .then(html => {
+    .then((response) => response.text())
+    .then((html) => {
       // Send the HTML content to the content script
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { htmlContent: html });
       });
     })
-    .catch(error => {
+    .catch((error) => {
       console.error("Error:", error.message);
     });
 }
 
 function showPhishingDetectedNotification(url) {
   chrome.notifications.create({
-    type: 'basic',
-    iconUrl: 'images/icon-48.png',
-    title: `Phishing Detector\nPhishing Site Detected!`,
+    type: "basic",
+    iconUrl: "images/icon-48.png",
+    title: `Phishing Detector Notification\nPhishing Site Detected!`,
     message: `${url}`,
-    buttons: [
-      { title: 'Close Page' }
-    ],
-    priority: 0
+    buttons: [{ title: "Close Page" }],
+    requireInteraction: true,
+    priority: 0,
   });
 }
 
 function showRealSiteNotification() {
   chrome.notifications.create({
-    type: 'basic',
-    iconUrl: 'images/icon-48-success.png',
-    title: `Phishing Detector\nSafe to Log In`,
+    type: "basic",
+    iconUrl: "images/icon-48-success.png",
+    title: `Phishing Detector Notification\nSafe to Log In`,
     message: `This site appears to be safe.`,
-    priority: 0
+    priority: 0,
+  });
+}
+
+function showWarningNotification() {
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "images/icon-48-warning.png",
+    title: `Phishing Detector Notification\nHigh risk of data theft `,
+    message: `It is not recommended to enter your banking information on this page.`,
+    buttons: [{ title: "Close Page" }],
+    requireInteraction: true,
+    priority: 0,
   });
 }
--- content.js
+++ content.js
@@ -1,123 +1,292 @@
-console.log("Content script loaded."); // You can log messages to the console for testing.
+class NotificationManager {
+  constructor() {
+    this.phishingNotificationShown = false;
+    this.realPageNotificationShown = false;
+    this.warningNotificationShown = false;
+  }
 
-// Get the source code of the web page
-let phishingNotificationShown = false;
-let realpageNotificationShown = false;
-let scriptHasRun = false
+  showPhishingNotification(url) {
+    if (!this.phishingNotificationShown) {
+      this.phishingNotificationShown = true;
+      chrome.runtime.sendMessage({
+        showPhishingPageNotification: true,
+        url: url,
+      });
+    }
+  }
 
-function sourceCodeCheck(sourceCode, currentUrl) {
+  showWarningNotification() {
+    if (!this.warningNotificationShown) {
+      this.warningNotificationShown = true;
+      chrome.runtime.sendMessage({ showWarningPageNotification: true });
+    }
+  }
+
+  showRealPageNotification() {
+    if (!this.realPageNotificationShown) {
+      this.realPageNotificationShown = true;
+      chrome.runtime.sendMessage({ showRealPageNotification: true });
+    }
+  }
+
+  resetRealPageNotification() {
+    this.realPageNotificationShown = false;
+  }
+}
+
+const notificationManager = new NotificationManager();
+
+let domainCheckScriptHasRun = false;
+let sourceCodeScriptHasRun = false;
+let calculateRiskScriptHasRun = false;
+
+const sourceCode = document.documentElement.outerHTML;
+const currentUrl = window.location.href;
+runChecks(sourceCode, currentUrl, notificationManager);
+
+function runChecks(sourceCode, currentUrl, notificationManager) {
+  let riskScore = 0;
+
+  // Call the sourceCodeCheck function and handle notifications
+  if (!sourceCodeScriptHasRun) {
+    const sourceCodeCheck_result = sourceCodeCheck(
+      sourceCode,
+      currentUrl,
+      notificationManager
+    );
+    if (!sourceCodeCheck_result) {
+      // Send message to background.js to start whoisQuery
+      chrome.runtime.sendMessage({ whoisQuery: true, domain_url: currentUrl });
+
+      // Start domainCheck with coming message from background.js
+      chrome.runtime.onMessage.addListener(function (
+        message,
+        _sender,
+        _sendResponse
+      ) {
+        if (!domainCheckScriptHasRun) {
+          const domainAge = domainCheck(message);
+          domainCheckScriptHasRun = true;
+
+          // Calculate the risk based on source code and add it to the riskScore
+          if (!calculateRiskScriptHasRun) {
+            const sourceCodeRisk = calculateRisk(sourceCode, currentUrl);
+            riskScore += sourceCodeRisk;
+
+            // Add domain age-based risk
+            if (domainAge <= 30) {
+              riskScore += 50;
+            }
+
+            if (riskScore >= 70) {
+              notificationManager.showWarningNotification();
+            }
+            calculateRiskScriptHasRun = true;
+            console.log("Final Risk Score: " + riskScore);
+          }
+        }
+      });
+    }
+    sourceCodeScriptHasRun = true;
+  }
+}
+
+// Add a listener for messages from the background script
+
+function sourceCodeCheck(sourceCode, currentUrl, notificationManager) {
   const banks = [
     {
-      assets: "assets.garanti",
+      assets: [
+        "/assets/img/logo-garantibbva.png",
+        "assets.garanti",
+        "GT.temporary",
+        "logo-garantibbva-2x.png",
+        "sube.garantibbva.com.tr/isube/login/login/passwordentrypersonal-tr",
+        "sube.garantibbva.com.tr/isube/login/login/passwordentrycorporate-tr",
+      ],
       url: "sube.garantibbva.com.tr",
     },
     {
-      assets: "/WebApplication.UI/entrypoint.aspx?T",
+      assets: ["/WebApplication.UI/entrypoint.aspx?T"],
       url: "internetsubesi.akbank.com",
     },
     {
-      assets: "/ibank/static/css",
+      assets: ["/ibank/static/css"],
       url: "isube.anadolubank.com",
     },
     {
-      assets: "/vendors.bundle.web.92185d4429e4b3c998a3.3.1.7.402.js",
+      assets: ["/vendors.bundle.web.92185d4429e4b3c998a3.3.1.7.402.js"],
       url: "internetbankaciligi.fibabanka.com.tr",
     },
     {
-      assets: "sube.sekerbank.com.tr/static",
+      assets: ["sube.sekerbank.com.tr/static"],
       url: "sube.sekerbank.com.tr",
     },
     {
-      assets: "assets/css/teb-style.css",
+      assets: ["assets/css/teb-style.css"],
       url: "esube.teb.com.tr/",
     },
     {
-      assets: "/Internet/IntSubeJS",
+      assets: ["/Internet/IntSubeJS"],
       url: "isbank.com.tr/Internet/",
     },
     {
-      assets: "inventus.yapikredi",
+      assets: ["inventus.yapikredi"],
       url: "internetsube.yapikredi.com.tr",
     },
     {
-      assets: "styles.f13e9bd3a25ecbd90517.css",
+      assets: ["styles.f13e9bd3a25ecbd90517.css"],
       url: "acikdeniz.denizbank.com/",
     },
     {
-      assets: "/Content/Themes/FinansbankTheme",
+      assets: ["/Content/Themes/FinansbankTheme"],
       url: "internetsubesi.qnbfinansbank.com",
     },
   ];
 
+  const skipPhishingNotificationUrls = [
+    "https://www.garantibbva.com.tr/",
+    "https://www.akbank.com/tr-tr/sayfalar/default.aspx",
+    "https://www.isbank.com.tr/",
+    "https://www.yapikredi.com.tr/",
+    "https://www.denizbank.com/",
+    "https://www.qnbfinansbank.com/",
+    "https://www.teb.com.tr/",
+    "https://www.sekerbank.com.tr/",
+    "https://www.fibabanka.com.tr/",
+    "https://www.anadolubank.com.tr/sizin-icin",
+    // Add more URLs to skip here
+  ];
+
+  const isSpesifiedURL = skipPhishingNotificationUrls.includes(currentUrl);
+
   for (const bank of banks) {
-    if (sourceCode.includes(bank.assets) && !currentUrl.includes(bank.url)) {
-      if (!phishingNotificationShown) {
-        phishingNotificationShown = true;
-        chrome.runtime.sendMessage({ showPhishingPageNotification: true, url: currentUrl });
+    const { assets, url } = bank;
+    const assetsFound = assets.some((asset) => sourceCode.includes(asset));
+
+    if (assetsFound) {
+      if (currentUrl.includes(url)) {
+        notificationManager.resetRealPageNotification();
+        notificationManager.showRealPageNotification();
+        sourceCodeScriptHasRun = true;
+        return true;
+      } else {
+        if (!isSpesifiedURL) {
+          notificationManager.showPhishingNotification(currentUrl);
+        }
+        sourceCodeScriptHasRun = true;
+        return true;
       }
-      return true;
-    }
-    if (sourceCode.includes(bank.assets) && currentUrl.includes(bank.url)) {
-      if (!realpageNotificationShown) {
-        realpageNotificationShown = true;
-        chrome.runtime.sendMessage({ showRealPageNotification: true });
-      }
-      return true;
     }
   }
-
+  sourceCodeScriptHasRun = true;
   return false;
 }
 
-
-function processMessage(message) {
-  
+function domainCheck(message) {
   if (message.htmlContent) {
-    // Parse the HTML content sent from the background script
     const html = message.htmlContent;
-
-    // Create a temporary element to parse the HTML
-    const tempElement = document.createElement('div');
+    const tempElement = document.createElement("div");
     tempElement.innerHTML = html;
 
-    const registryDataBlock = tempElement.querySelector("#registryData");
+    const registryDataBlock = tempElement.querySelector(".whois-data");
 
-    /* registry kontrol - domain age check */
     if (registryDataBlock) {
       const registryBlockText = registryDataBlock.textContent;
-      const indexofCreation = registryBlockText.indexOf("Creation");
-      const createdOn = registryBlockText.substring(indexofCreation + 15, indexofCreation + 25);
+      const indexofCreation = registryBlockText.indexOf("Creat");
+      const createdOn = registryBlockText.substring(
+        indexofCreation + 15,
+        indexofCreation + 25
+      );
 
       const date1 = new Date();
       const date2 = new Date(createdOn);
       const diffTime = Math.abs(date2 - date1);
       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      
       console.log(diffDays + " days");
-      console.log("Created on:", createdOn);
+      console.log("Created on:", createdOn)
+      return diffDays;
     } else {
       console.log("#registryData element not found in the HTML.");
+      return 0;
     }
   }
 
-  // Set the scriptHasRun variable to true to indicate that the script has run
-  scriptHasRun = true;
+  domainCheckScriptHasRun = true;
+  return 0;
 }
 
-const sourceCode = document.documentElement.outerHTML;
-const currentUrl = window.location.href;
-const sourceCodeCheck_result = sourceCodeCheck(sourceCode, currentUrl);
+function calculateRisk(sourceCode, currentUrl) {
+  // Define your query keywords with associated weights
+  const queryKeywords = [
+    {
+      keywords: [
+        { keyword: "Internet Banka", weight: 10 },
+        { keyword: "İnternet Banka", weight: 10 },
+        { keyword: "İnternet Şube", weight: 10 },
+        { keyword: "Internet Sube", weight: 10 },
+        { keyword: "custno", weight: 5 },
+        { keyword: "loginForm", weight: 3 },
+        { keyword: "form", weight: 3 },
+        { keyword: "Form", weight: 3 },
+        { keyword: "müşteri num", weight: 5 },
+        { keyword: "musteri num", weight: 5 },
+        { keyword: "T.C. kimlik", weight: 5 },
+        { keyword: "T.C. Kimlik", weight: 5 },
+        { keyword: "TC Kimlik", weight: 5 },
+        { keyword: "TC kimlik", weight: 5 },
+        { keyword: "Müşteri Num", weight: 5 },
+        { keyword: "password", weight: 5 },
+        { keyword: "formField", weight: 3 },
+        { keyword: "Parola", weight: 10 },
+        { keyword: "parola", weight: 10 },
+        { keyword: "Beni Hatırla", weight: 5 },
+        { keyword: "Müşteri / T.C", weight: 10 },
+        { keyword: "Müşteri/T.C", weight: 10 },
+        { keyword: "TCKN", weight: 10 },
+        { keyword: "Müşteri/T.C", weight: 10 },
+        { keyword: "Müşteri/T.C", weight: 10 },
+        // Add more keywords with their weights
+      ],
+      urls: [
+        //"sube.garantibbva.com.tr",
+        "internetsubesi.qnbfinansbank.com",
+        "acikdeniz.denizbank.com/",
+        "internetsube.yapikredi.com.tr",
+        "isbank.com.tr/Internet/",
+        "esube.teb.com.tr/",
+        "sube.sekerbank.com.tr",
+        "internetbankaciligi.fibabanka.com.tr",
+        "isube.anadolubank.com",
+        "internetsubesi.akbank.com",
+        // Add more URLs
+      ],
+    },
+    // You can define additional keyword sets here
+  ];
 
-if (!sourceCodeCheck_result) {
-  chrome.runtime.sendMessage({ calculateRisk: true , domain_url: currentUrl })
-  // Function to process the message and extract data
-  // Listen for messages from the background script only if the script hasn't run yet
-  if (!scriptHasRun) {
-    chrome.runtime.onMessage.addListener(function (message, _sender, _sendResponse) {
-      // Check if the script has already run before processing the message
-      if (!scriptHasRun) {
-        processMessage(message);
+  let keywordCatch = 0; // Initialize the keywordCatch variable
+
+  // Check if currentUrl matches any of the specified URLs
+  const isUrlSpecified = queryKeywords.some((queryKeyword) =>
+    queryKeyword.urls.includes(currentUrl)
+  );
+
+  if (!isUrlSpecified) {
+    // If the currentUrl is not in the specified list, proceed with keyword analysis
+    for (const queryKeyword of queryKeywords) {
+      const { keywords, urls } = queryKeyword;
+
+      for (const keywordObj of keywords) {
+        const { keyword, weight } = keywordObj;
+
+        // Check if the keyword is found in the sourceCode
+        if (sourceCode.includes(keyword)) {
+          keywordCatch += weight; // Increase keywordCatch by the keyword's weight
+        }
       }
-    });
+    }
   }
-};
+  calculateRiskScriptHasRun = true;
+  return keywordCatch; // Return the calculated risk score
+}
